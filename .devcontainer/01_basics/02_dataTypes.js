3dfcaad0346f4205845012c6571a4c021b7cfc3f
khushbu07 <113490172+khushbupati@users.noreply.github.com>
2023-08-18 13:31:00 +0000

    container config done

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+// treat all js code as a newer version
+// alert(3+3),this will not work because we are using node js ,not the browser
+console.log(3
+    +
+     3);  //code must be readable
+
+console.log("khush");
+
+
+let name="khush"
+let age =18
+let isLoggedIn = false
+
+
+// number 2=> to power 53
+//bigint
+//string=>""
+
+
