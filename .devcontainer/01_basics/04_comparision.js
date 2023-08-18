84f030908be0c91eaf5e5016cd1a6b47ac1691fe
khushbu07 <113490172+khushbupati@users.noreply.github.com>
2023-08-18 13:49:12 +0000

    container config done

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+// NaN means not a number
+
+console.log("2" >1 );
+console.log("02">1 );
+
+console.log(null  > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+//  === this checks the datatype and not do the conversion
+// == this is used for the conversion perpose
+console.log(undefined < 0 );
+console.log(undefined > 0 );
