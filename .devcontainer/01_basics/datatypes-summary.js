2723cbba7f0057da01411bffe8bd3ca4f7824b6e
khushbu07 <113490172+khushbupati@users.noreply.github.com>
2023-08-19 03:14:45 +0000

    container config done

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+const score = 100
+const scoreValue =100.3
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 24555655677668787n
+
+
+
+// reference (not primitive)
+
+// array , objects , functions
+
+const heros =["shaktiman", "naagraj", "doga"];
+let myObj ={
+    name: "khush",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+console.log(typeof heros);
+console.log(typeof myFunction);
+console.log(typeof scoreValue);
+console.log(typeof bigNumber);
+
+
+
